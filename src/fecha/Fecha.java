048d410fbce22eece2fe8464f59fd7aaa408a403
Ana Montero Rodríguez <ana.montero@info2>
2025-03-06 09:39:59 +0100

    intento subida javadoc, se han borrado las ramas al cerrar el cmd, intento 1

--- src/fecha/Fecha.java
+++ src/fecha/Fecha.java
@@ -1,28 +1,6 @@
 package fecha;
 
 /**
-<<<<<<< HEAD
- * Clase fecha
- */
-public class Fecha {
-	/**
-	 * Atributo privado d
-	 */
-	private int d; // d�a
-
-	/**
-	 * Atributo privado m
-	 */
-	private int m; // mes
-
-	/**
-	 * Atributo a
-	 */
-	private int a; // a�o
-
-	/**
-	 * Método fecha el cual no hace nada
-=======
  * Clase Fecha
  */
 public class Fecha {
@@ -33,18 +11,13 @@ public class Fecha {
 
 	/**
 	 * Metodo fecha
->>>>>>> pmd-refactor
 	 */
 	public Fecha() {
 
 	}
 
 	/**
-<<<<<<< HEAD
-	 * Constructor de fecha con sus diferentes parámetros
-=======
 	 * Constructor con los atributos de la clase
->>>>>>> pmd-refactor
 	 * 
 	 * @param dia
 	 * @param mes
@@ -57,15 +30,9 @@ public class Fecha {
 	}
 
 	/**
-<<<<<<< HEAD
-	 * Método para comprobar si la fecha es correcta o no
-	 * 
-	 * @return true o false dependiendo del resultado
-=======
 	 * Método para hayar si la fecha es correcta o no
 	 * 
 	 * @return true o false dependiendo de la validez de la operacion
->>>>>>> pmd-refactor
 	 */
 	public boolean fechaCorrecta() {
 		boolean diaCorrecto;
@@ -97,28 +64,17 @@ public class Fecha {
 	/**
 	 * Método esBisiesto. Solo lo usa fechaCorrecta, por eso es privado
 	 * 
-<<<<<<< HEAD
-	 * @return true o false dependiendo si el mes es bisiesto o no
-=======
 	 * @return true o false dependiendo del resultado de si es bisiesto o no
->>>>>>> pmd-refactor
 	 */
 	private boolean esBisiesto() {
 		return variableA % 4 == 0 && variableA % 100 != 0 || variableA % 400 == 0;
 	}
 
 	/**
-<<<<<<< HEAD
-	 * Método diaSiguiente que aumenta en 1 el día
-	 */
-	public void diaSiguiente() {
-		d++;
-=======
 	 * Método diaSiguiente que aumenta el dia en 1
 	 */
 	public void nextDay() {
 		varaibleD++;
->>>>>>> pmd-refactor
 		if (!fechaCorrecta()) {
 			varaibleD = 1;
 			variableM++;
@@ -130,7 +86,7 @@ public class Fecha {
 	}
 
 	/**
-	 * Método toString
+	 * Mtodo toString
 	 */
 	public String toString() {
 		String resultado = "";
